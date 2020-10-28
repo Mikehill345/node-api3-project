ce6a111772dcf09f39420a0e93115f4cd614c20d
mike hill <michael-hill@lambdastudents.com>
2020-10-28 18:32:33 -0400

    finished mvp

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
-
+const userRouter = require('./users/userRouter')
 const server = express();
 
+server.use(express.json())
+server.use(userRouter)
+
+const logger = (req, res, next) => {
+  let time = new Date()
+  res.append('Something', 'something')
+  console.log(`${req.method} ${req.url}`, time)
+  next()
+}
+server.use(logger)
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>Sanity Check</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+
+
+
+
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,106 @@
 const express = require('express');
-
+const User = require('./userDb')
 const router = express.Router();
+const Post = require('../posts/postDb')
 
-router.post('/', (req, res) => {
-  // do your magic!
+
+router.post('/api/users',[validateUser], (req, res) => {
+  User.insert(req.body)
+  .then((data) => {
+    res.status(201).json(data)
+  }).catch((err) => {
+    res.status(500).json('stop breaking it')
+  })
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/api/users/:id/posts',[validateUserId, validatePost], (req, res) => {
+  const newPost = {
+    user_id: req.params.id,
+    text: req.body.text,
+  };
+  Post.insert(newPost)
+  .then((data) => {
+    res.status(201).json(data)
+  }).catch((err) => {
+    res.status(500).json({ message: 'Something went horribly wrong' })
+  })
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get('/api/users', (req, res, next) => {
+  User.get() 
+  .then((data) => {
+    res.status(200).json(data)
+  }).catch((err) => {
+    next('you suck ass')
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/api/users/:id',validateUserId, (req, res) => {
+  res.status(200).json(req.user)
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/api/users/:id/posts',validateUserId, (req, res) => {
+  User.getUserPosts(req.params.id)
+  .then((data) => {
+    res.status(200).json(data)
+  }).catch((err) => {
+    res.status(500).json('it borked')
+  })
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/api/users/:id',validateUserId, (req, res) => {
+  User.remove(req.params.id)
+  .then((data) => {
+    res.status(200).json('You successfully deleted this guy')
+  }).catch((err) => {
+    res.status(500).json('it messed up')
+  })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/api/users/:id',[validateUserId, validateUser], (req, res) => {
+  User.update(req.params.id, req.body)
+  .then((data) => {
+    res.status(200).json(data)
+  }).catch((err) => {
+    res.status(500).json('it messed up')
+  })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const { id } = req.params
+  User.getById(id)
+  .then((data) => {
+    if(data){
+      req.user = data
+      next()
+    } else {
+      res.status(400).json({message: 'invalid user id'})
+    }
+  }).catch((err) => {
+    next()
+  })
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if(!req.body){
+    res.status(400).json('missing user data')
+  } else if(!req.body.name){
+    res.status(400).json('missing required name field')
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if(!req.body.text){
+    res.status(400).json('Please fill out text field')
+  } else {
+    next()
+  }
 }
-
+router.use((error, req, res, next) => {
+  res.status(error.code).json({message: error.message})
+})
 module.exports = router;
